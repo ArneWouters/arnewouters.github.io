c627e40b147076646ec2b626c87872296a7def90
Arne Wouters <arne@wouters.co>
2021-06-30 22:39:35 +0200

    First commit new vue project

---
+++ src/components/MobileNav.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="nav">
+    <div class="nav-menu">
+      <ul class="nav-menu-list">
+        <li class="nav-list-item"><router-link to="/" class="routerlink">About</router-link></li>
+        <li class="nav-list-item">Contact Me</li>
+        <li class="nav-list-item">PGP Key</li>
+        <li class="nav-list-item"><router-link to="/github" class="routerlink"><github class="icon"/></router-link></li>
+        <li class="nav-list-item"><linkedin class="icon"/></li>
+        <li class="nav-list-item"><twitter class="icon"/></li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import { Linkedin, Github, Twitter } from 'mdue';
+
+export default {
+  components: {
+    Linkedin,
+    Github,
+    Twitter,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  .nav-menu {
+    color: #222;
+    width: 100%
+  }
+
+  .nav-menu-list {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    margin: 0;
+
+    .nav-list-item {
+      padding: 10px 0;
+      text-align: center;
+      width: 50%;
+
+      .icon {
+        font-size: 20px;
+      }
+    }
+  }
+}
+
+.routerlink {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card">
+    <div class="card-title">
+      <h1>This is an about page</h1>
+    </div>
+    <div class="card-content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus congue enim, sed condimentum felis dictum et. Cras condimentum dictum mattis. Duis malesuada dui tristique nisl eleifend porta. Nulla dictum mauris est, vitae facilisis massa ornare mattis. Nulla condimentum tellus at tellus fringilla, eu consequat libero venenatis. Fusce aliquam molestie diam, non iaculis dolor euismod eget. Donec sodales orci vehicula turpis condimentum imperdiet. Etiam suscipit orci et suscipit malesuada.</p>
+    </div>
   </div>
 </template>
