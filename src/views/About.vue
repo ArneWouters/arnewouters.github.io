a66836e548f02ee445f317e99ae4917defecd4f4
Arne Wouters <arne@wouters.co>
2021-06-30 23:37:29 +0200

    Update About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="card">
     <div class="card-title">
-      <h1>This is an about page</h1>
+      <h1>Hi, I'm Arne Wouters.</h1>
     </div>
     <div class="card-content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus congue enim, sed condimentum felis dictum et. Cras condimentum dictum mattis. Duis malesuada dui tristique nisl eleifend porta. Nulla dictum mauris est, vitae facilisis massa ornare mattis. Nulla condimentum tellus at tellus fringilla, eu consequat libero venenatis. Fusce aliquam molestie diam, non iaculis dolor euismod eget. Donec sodales orci vehicula turpis condimentum imperdiet. Etiam suscipit orci et suscipit malesuada.</p>
+      <p>I'm a software engineer from Belgium. Currently working towards a MSc degree in Computer Science at KU Leuven. </p>
     </div>
   </div>
 </template>
